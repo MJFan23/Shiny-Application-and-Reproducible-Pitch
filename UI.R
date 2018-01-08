fbcd635ebd0d3196cbddf0653c4123b7f7806749
MJFan23 <LMayer23@gmail.com>
2018-01-08 12:32:46 -0500

    Add files via upload

---
+++ UI.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+##Load Data
+data <- read.csv("/Users/Lauren/Documents/Data Science Coursera/Stat4/EmployeeData.csv")
+
+
+ui <- fluidPage(
+  ## Staging UI
+  shinyServer(
+    pageWithSidebar(
+      headerPanel("Data Inputs"),
+      sidebarPanel(
+        selectInput("Distribution","Please Select Distribution Type",
+                    choices=c("Normal","Exponential")),
+        ##selectInput("Occupation", "Occuption:",  
+        ## choices=colnames(data)), 
+        sliderInput("sampleSize", "Please Select Number of Employees:",
+                    min=620000, max=35000000, value=65000, step=50000),
+        conditionalPanel(condition="input.Distribution == 'Normal'",
+                         textInput("mean", "Please Select the Number of Firms:", 10000),
+                         textInput("sd","Please Select Standard Deviation:", 3)),
+        conditionalPanel(condition="input.Distribution == 'Exponential'",
+                         textInput("lambda","Please Select Exponential Lambda:", 1))
+      ),
+      mainPanel(
+        plotOutput("myPlot")
+      )
+    )
+  )
+)
---
+++ server.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+##Load Data
+data <- read.csv("/Users/Lauren/Documents/Data Science Coursera/Stat4/EmployeeData.csv")
+summary(data)
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  output$myPlot <- renderPlot({
+    distType <- input$Distribution
+    size <- input$sampleSize
+    
+    if(distType == "Normal"){
+      randomVec <- rnorm(size, mean = as.numeric(input$mean), 
+                         sd = as.numeric(input$sd))
+    }
+    else {
+      randomVec <- rexp (size, rate = 1/ as.numeric(input$lambda))
+    }
+    hist(randomVec,freq = F, main="Distribution of Employees in US",
+         xlab = "Number of Firms", ylab = "Density of Firms"
+         ,col = "blue")
+  }) 
+}
